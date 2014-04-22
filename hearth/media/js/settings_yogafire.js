4065c08fa26357ba74d001705bd017d8ff229a87
Kevin Ngo <ngoke@mozilla.com>
2014-04-21 17:10:22 -0700

    disable payments :fire:, enable :fries:lytics

---
+++ hearth/media/js/settings_yogafire.js
@@ -0,0 +1,8 @@
+define('settings_yogafire', [], function() {
+    return {
+        payments_enabled: false,
+        potatolytics_enabled: true,
+        newsletter_enabled: false,
+        tracking_enabled: true
+    };
+});
