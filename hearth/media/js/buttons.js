ceca319c3ff72ea1a13823071fcd03e518634f12
Kevin Ngo <ngoke@mozilla.com>
2014-05-09 15:37:13 -0700

    remove receipt/record logic in install (bug 1007946)

--- hearth/media/js/buttons.js
+++ hearth/media/js/buttons.js
@@ -51,48 +51,15 @@ define('buttons',
         var product_name = product.name;
         console.log('Install requested for', product_name);
 
-        // TODO: Have the API possibly return this (bug 889501).
-        product.receipt_required = (product.premium_type !== 'free' &&
-                                    product.premium_type !== 'free-inapp' &&
-                                    !require('settings').simulate_nav_pay);
-
-        // If it's a paid app, it's unsupported for now.
-        if (product.receipt_required) {
-            console.log('Install cancelled; product is not free');
-            notify({message: gettext('Payment cancelled.')});
-        }
-
-        // If there isn't a user object on the app, add one.
-        if (!product.user) {
-            console.warn('User data not available for', product_name);
-            product.user = {
-                purchased: false,
-                installed: false,
-                developed: false
-            };
-        }
-
         // Create a master deferred for the button action.
         var def = require('defer').Deferred();
         // Create a reference to the button.
         var $this = $button || $(this);
 
-        // If the user has already purchased the app, we do need to generate
-        // another receipt but we don't need to go through the purchase flow again.
-        if (user.has_purchased(product.id)) {
-            product.payment_required = false;
-        }
-
-        if (product.payment_required) {
-            // The app requires a payment.
-            notify({message: gettext('Payment cancelled.')});
-            console.log('Purchase flow rejected for', product_name);
-        } else {
-            // There's no payment required, just start install.
-            console.log('Starting app installation for', product_name);
-            // Start the app's installation.
-            start_install();
-        }
+        // There's no payment required, just start install.
+        console.log('Starting app installation for', product_name);
+        // Start the app's installation.
+        start_install();
 
         function start_install() {
             // Track the search term used to find this app, if applicable.
@@ -111,54 +78,7 @@ define('buttons',
                  .html('<span class="spin"></span>')
                  .addClass('spinning');
 
-            // If the app has already been installed by the user and we don't
-            // need a receipt, just start the app install.
-            if (user.has_installed(product.id) && !product.receipt_required) {
-                console.log('Receipt not required (skipping record step) for', product_name);
-                return do_install();
-            }
-
-            // This is the data needed to record the app's install.
-            var api_endpoint = urls.api.url('record_' + (product.receipt_required ? 'paid' : 'free'));
-            var post_data = {app: product.id, chromeless: +require('capabilities').chromeless};
-
-            // If we don't need a receipt to perform the installation...
-            if (!product.receipt_required) {
-                // Do the install immediately.
-                do_install().done(function() {
-                    // ...then record the installation.
-                    requests.post(api_endpoint, post_data);
-                    // We don't care if it fails or not because the user has
-                    // already installed the app.
-                });
-                return;
-            }
-
-            // Let the API know we're installing.
-            requests.post(api_endpoint, post_data).done(function(response) {
-                // If the server returned an error, log it and reject the deferred.
-                if (response.error) {
-                    console.log('Server returned error: ' + response.error);
-                    def.reject();
-                    return;
-                }
-                do_install({data: {'receipts': [response.receipt]}});
-
-            }).fail(function() {
-                utils_local.checkOnline().done(function() {
-                    notify({
-                        // L10n: The app's installation has failed, but the problem is temporary.
-                        message: gettext('Sorry, there was an issue contacting server for install. Please try again later.')
-                    });
-                }).fail(function() {
-                    notify({message: settings.offline_msg});
-                });
-                def.reject();
-            });
-        }
-
-        function do_install(data) {
-            return apps.install(product, data || {}).done(function(installer) {
+            return apps.install(product, {}).done(function(installer) {
                 // Update the cache to show that the user installed the app.
                 user.update_install(product.id);
                 // Bust the cache for the My Apps page.
