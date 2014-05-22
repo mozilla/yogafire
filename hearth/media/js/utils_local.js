2be8ef6ea3b968cb8f61ec2ca4493c2b8ede772a
Mathieu Pillard <mpillard@mozilla.com>
2014-05-22 15:20:19 +0200

    Use mozTCPSocket to detect online/offline status (bug 1014508)

--- hearth/media/js/utils_local.js
+++ hearth/media/js/utils_local.js
@@ -1,19 +1,20 @@
-define('utils_local', ['defer', 'log', 'urls', 'z'], function(defer, log, urls, z) {
+define('utils_local', ['defer', 'log', 'settings', 'urls', 'z'], function(defer, log, settings, urls, z) {
     var console = log('utils_local');
     var check_interval;
     var timeout = 10000;  // 10 seconds.
 
-    function offline(def) {
+    function offline(socket, def) {
         if (z.onLine) {
             // Fire event for going offline.
             console.log('Offline detected.');
             z.win.trigger('offline_detected');
             z.onLine = false;
         }
+        reset_socket(socket);
         def.reject();
     }
 
-    function online(def) {
+    function online(socket, def) {
         if (!z.onLine) {
             // Fire event for going online.
             // Fire event to start loading images.
@@ -21,36 +22,32 @@ define('utils_local', ['defer', 'log', 'urls', 'z'], function(defer, log, urls, 
             z.win.trigger('online_detected image_defer');
             z.onLine = true;
         }
+        reset_socket(socket);
         def.resolve();
+    }
+
+    function reset_socket(socket) {
+        socket.onopen = null;
+        socket.onerror = null;
+        socket.close();
     }
 
     function checkOnline() {
         // `navigator.onLine` sucks (bug 654579/756364).
         // Protip: to mock offline, do "require('z').onLine = false" (and gg money) in console.
         var def = defer.Deferred();
-        var xhr = new XMLHttpRequest();
-        var url = urls.media('fireplace/img/grain.png?') + +new Date();
-
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                console.log('Got status ' + xhr.status + ' when requesting ' + url);
-                if (xhr.status === 0) {
-                    offline(def);
-                } else {
-                    online(def);
-                }
-            }
+        // FIXME: try/catch and do something else on desktop since the permission won't be there.
+        var host = (new URL(settings.cdn_url)).host;
+        var port = 80;
+        var socket = navigator.mozTCPSocket.open(host, port);
+        console.log('Calling navigator.mozTCPSocket.open(' + host + ',' + port + ')');
+        socket.onerror = function(e) {
+            offline(socket, def);
+        };
+        socket.onopen = function(e) {
+            online(socket, def);
         };
 
-        xhr.open('HEAD', url, true);
-        xhr.timeout = timeout;
-
-        xhr.ontimeout = function() {
-            console.log('Timeout when requesting ' + url);
-            offline(def);
-        };
-
-        xhr.send();
         return def.promise();
     }
 
